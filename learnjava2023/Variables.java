626c07a37440e769483ee6ad95688146f3921db3
Mert Temizer <mert10temizer@gmail.com>
2023-11-09 14:40:30 +0300

    Add files via upload

---
+++ learnjava2023/Variables.java
@@ -0,0 +1,14 @@
+package learnjava2023;
+
+public class Variables {
+
+	public static void main(String[] args) {
+			//camelCase
+		int luckyNumber=10;
+		String message="My lucky number";
+		System.out.println("My lucky number: 9");
+		System.out.println(message + luckyNumber);
+
+	}
+
+}
