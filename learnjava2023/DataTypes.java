6eb572e60fe75186c4acc871d9ad7ace47579357
Mert Temizer <mert10temizer@gmail.com>
2023-11-09 14:49:15 +0300

    Add files via upload

---
+++ learnjava2023/DataTypes.java
@@ -0,0 +1,17 @@
+package learnjava2023;
+
+public class DataTypes {
+
+	public static void main(String[] args) {
+		
+		int num = 5;
+		
+		double num1 = 12.5;
+		
+		char character = 'M';
+		
+		boolean isRight = false;
+		
+	}
+
+}
