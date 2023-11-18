80ea3d928f287282bfe57ebc69e017f9d0175cbc
Mert Temizer <mert10temizer@gmail.com>
2023-11-18 22:34:33 +0300

    Add files via upload

---
+++ learnjava2023/WhileLoop.java
@@ -0,0 +1,16 @@
+package learnjava2023;
+
+public class WhileLoop {
+
+	public static void main(String[] args) {
+		
+		int i = 1;
+		
+		while(i <= 10) {
+			System.out.println(i);
+			i += 2;
+		}
+		System.out.println("The Loop is over.");
+	}
+
+}
