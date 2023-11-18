8be3bf2dff20c9b2eafde7717d9582b07899e733
Mert Temizer <mert10temizer@gmail.com>
2023-11-18 21:50:19 +0300

    Add files via upload

---
+++ learnjava2023/Conditionals.java
@@ -0,0 +1,24 @@
+package learnjava2023;
+
+public class Conditionals {
+
+	public static void main(String[] args) {
+		
+		int number = 24;
+		
+		if(number > 20) {
+			
+			System.out.println("Number is bigger than 20");
+		
+		}else if(number == 20){
+			
+			System.out.println("Number is equal to 20");
+			
+		}else {
+			
+			System.out.println("Number lower than 20");
+			
+		}
+	}
+
+}
